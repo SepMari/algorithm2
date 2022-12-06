9583ed09870e5d3e60cae0d49c0c5f8e573814f2
lldv <48766898+SepMari@users.noreply.github.com>
2022-12-06 18:14:27 +0300

    Выполненое дз - Алгоритмы. Часть 3 (рекурсивная сортировка)

--- src/arrayList/IntegerListImpl.java
+++ src/arrayList/IntegerListImpl.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 
 public class IntegerListImpl implements IntegerList {
 
-    private final Integer[] storage;
+    private Integer[] storage;
     private int size;
 
     public IntegerListImpl() {
@@ -18,7 +18,7 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public Integer add(Integer item) {
-        validateSize();
+        growIfNeeded();
         validateItem(item);
         storage[size++] = item;
         return item;
@@ -26,7 +26,7 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public Integer add(int index, Integer item) {
-        validateSize();
+        growIfNeeded();
         validateItem(item);
         validateIndex(index);
 
@@ -160,15 +160,38 @@ public class IntegerListImpl implements IntegerList {
     }
 
     private void sort(Integer[] arr) {
-        for (int i = 1; 1 < arr.length; i++) {
-            int temp = arr[i];
-            int j = i;
-            while (j > 0 && arr[j - 1] >= temp) {
-                arr[j] = arr[j - i];
-                j--;
-            }
-            arr[j] = temp;
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    //Быстрая сортировка
+    private void quickSort(Integer[] arr, int start, int end) {
+        if (start < end) {
+            int partitionIndex = partition(arr, start, end);
+
+            quickSort(arr, start, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
         }
+    }
+
+    private int partition(Integer[] arr, int start, int end) {
+        int pivot = arr[end];
+        int i = (start - 1);
+
+        for(int j = start; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swapElements(arr, i, j);
+            }
+        }
+
+        swapElements(arr, i + 1, end);
+        return i + 1;
+    }
+
+    private void swapElements(Integer[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     private boolean binarySearch(Integer[] arr, Integer item) {
@@ -189,5 +212,16 @@ public class IntegerListImpl implements IntegerList {
             }
         }
         return false;
+    }
+
+    private void grow() {
+        storage = Arrays.copyOf(storage, size + size / 2);
+    }
+
+
+    private void growIfNeeded() {
+        if (size == storage.length) {
+            grow();
+        }
     }
 }
